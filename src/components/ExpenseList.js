da6c9e0f09886b4830dac6678ef07a064093620a
hyunsu joo <hs9880@gmail.com>
2019-03-16 03:46:53 -0400

    connect Store&Compoenent with React-Redux

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import getVisibleExpense from '../selectors/expenses';
 const ExpenseList =(props)=>{
     return(<div>
        
         <h1>Expense List </h1>
-        {props.name}
+        <h3>Here is expense list page</h3>
+        <p>expense props: {props.expenses.length}</p>
+        <p>name : {props.name}</p>
+       
+        {props.expenses.map((expense)=>{
+                console.log('thisis expense',expense);
+               // return <ExpenseListItem  key={expense.id} id={expense.id} amount={expense.amount} description={expense.description} createdAt={expense.createdAt}/>
+               return <ExpenseListItem  key={expense.id} expense={expense}/>
+
+        })}
     </div>
     )
 
 }
 
-const ConnectedExpenseList = connect()(ExpenseList);
-    return{
-        name : 'Andrew'
-
-    }
-
-export default ExpenseList;
+const mapStateToProps=(state)=>{
+  // console.log(state.expenses);
+    return {
+       expenses: getVisibleExpense(state.expenses,state.filters)
+      // name : 'sue',
+       //filtersStore:state.filters
+    };
+};
+export default connect(mapStateToProps)(ExpenseList);
